6dfdad261997facde916f1e1a81596cf9809e5b0
Lisbeth Hernandez <105689509+Hgliss@users.noreply.github.com>
2025-10-25 14:55:23 -0600

    add: Configuracion de clase AuditoriaBase

---
+++ EmpresaAPI/Data/EmpresaContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EmpresaAPI.Models;
+
+
+namespace EmpresaAPI.Data
+{
+    public class EmpresaContext : DbContext
+    {
+        public EmpresaContext(DbContextOptions<EmpresaContext> options)
+            : base(options) { }
+        {
+        }
+
+    //tablas
+
+
+
+    //Configuraciones Personalizadas
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                if (typeof(AuditoriaBase).IsAssignableFrom(entityType.ClrType))
+                {
+                    //Usuario Creacion
+                    modelBuilder.Entity(entityType.ClrType)
+                        .HasOne(typeof(Usuario), "Usuario_Creacion")
+                        .WithMany()
+                        .HasForeignKey("Usuario_Creacion_Id")
+                        .OnDelete(DeleteBehavior.Restrict);
+
+
+                    //Usuario Modificacion
+                    modelBuilder.Entity(entityType.ClrType)
+                        .HasOne(typeof(Usuario), "Usuario_Modificacion")
+                        .WithMany()
+                        .HasForeignKey("Usuario_Modificacion_Id")
+                        .OnDelete(DeleteBehavior.Restrict);
+                }
+            }
+        }
+
+
+
+    }
+}
