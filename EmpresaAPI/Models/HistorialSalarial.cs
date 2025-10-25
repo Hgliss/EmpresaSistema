7e1c47ca00dcf09153ecc3673f0c6340371ca8a5
Lisbeth Hernandez <105689509+Hgliss@users.noreply.github.com>
2025-10-25 15:12:52 -0600

    add: Agregando Clase de Historial Salarial

---
+++ EmpresaAPI/Models/HistorialSalarial.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmpresaAPI.Models
+{
+    public class HistorialSalarial : AuditoriaBase
+    {
+        public int Id_Historial { get; set; }
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Salario_Anterior { get; set; }
+
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Salario_nuevo { get; set; }
+        public DateTime Fecha_Cambio {  get; set; }
+
+        [MaxLength(200)]
+        public string Motivo {  get; set; }
+
+        public int Empleado_Id {  get; set; }
+        public Empleado Empleado { get; set; } = null!;
+    }
+}
