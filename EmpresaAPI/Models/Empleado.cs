82799d08cb542010c4f6dad013c1fe253f73a8f3
Lisbeth Hernandez <105689509+Hgliss@users.noreply.github.com>
2025-10-25 14:56:09 -0600

    add: Agregando Clase Empleado

---
+++ EmpresaAPI/Models/Empleado.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmpresaAPI.Models
+{
+    public class Empleado : AuditoriaBase
+    {
+        public int Id_Empleado { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+
+        [Required]
+        [MaxLength(13)]
+        public string CUI { get; set; }
+        public DateTime Fecha_Ingreso { get; set; }
+        public decimal Salario_Actual { get; set; }
+        public DateTime? Fecha_Ultimo_Aumento { get; set; }
+        public DateTime? Fecha_baja {  get; set; }
+        public string Puesto { get; set; }
+        public string Jerarquia { get; set; }
+        public bool Estado { get; set; } = true;
+
+
+        public int Departamento_Id { get; set; }
+
+        public Departamento Departamento { get; set; }
+
+    }
+}
