93ca7fd45649a1cbac98f527591e8771e875f413
Lisbeth Hernandez <105689509+Hgliss@users.noreply.github.com>
2025-10-25 14:39:07 -0600

    add: Clase AuditoriaBase

---
+++ EmpresaAPI/Models/AuditoriaBase.cs
@@ -0,0 +1,15 @@
+﻿namespace EmpresaAPI.Models
+{
+    public abstract class AuditoriaBase
+    {
+        public DateTime Fecha_Creacion { get; set; } = DateTime.Now;
+        public DateTime? Fecha_Modificacion { get; set; }
+
+        public int Usuario_Creacion_Id { get; set; }
+        public int? Usuario_Modificacion_Id { get; set; }
+
+        public Usuario Usuario_Creacion { get; set; } = null!;
+        public Usuario Usuario_Modificacion { get; set; }
+
+    }
+}
