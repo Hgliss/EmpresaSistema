b15094241ce079c9fdbbd39ee8b33e6d2e3fa389
Lisbeth Hernandez <105689509+Hgliss@users.noreply.github.com>
2025-10-25 14:56:57 -0600

    Add: Env.Load

--- EmpresaAPI/Program.cs
+++ EmpresaAPI/Program.cs
@@ -1,3 +1,10 @@
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+using EmpresaAPI.Data;
+
+Env.Load();
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
