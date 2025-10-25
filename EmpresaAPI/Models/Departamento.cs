f0aebefa6bb52881c310fdf280a9839e18debcf7
Lisbeth Hernandez <105689509+Hgliss@users.noreply.github.com>
2025-10-25 15:02:29 -0600

    add: Agregando clase Departamento

---
+++ EmpresaAPI/Models/Departamento.cs
@@ -0,0 +1,12 @@
+﻿namespace EmpresaAPI.Models
+{
+    public class Departamento : AuditoriaBase
+    {
+        public int Id_Departamento { get; set; }
+        public string Nombre { get; set; }
+        public decimal Presupuesto { get; set; }
+        public bool Estado { get; set; } = true;
+
+        public ICollection<Empleado> Empleados { get; set; } = new List<Empleado>();
+    }
+}
