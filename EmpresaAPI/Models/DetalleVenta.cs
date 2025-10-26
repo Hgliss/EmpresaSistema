cd2e65fc3cc1c519c497eb599c838237171ca8eb
Lisbeth Hernandez <105689509+Hgliss@users.noreply.github.com>
2025-10-25 19:38:50 -0600

    add: Agregando Clase DetalleVenta

---
+++ EmpresaAPI/Models/DetalleVenta.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmpresaAPI.Models
+{
+    public class DetalleVenta : AuditoriaBase
+    {
+        public int Id_Detalle { get; set; }
+        public int Cantidad { get; set; }
+        [Column(TypeName ="decimal(10,2)")]
+        public decimal Precio { get; set; }
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Costo { get; set; }
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Subtotal { get; set; }
+
+        public int Venta_Id { get; set; }
+        public Venta Venta { get; set; } = null!;
+
+        public int Producto_Id { get; set; }
+        public Producto Producto { get; set; } = null!;
+
+    }
+}
