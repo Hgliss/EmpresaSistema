c28ae59c62b9567a0ce178470bf53556458f29e6
Lisbeth Hernandez <105689509+Hgliss@users.noreply.github.com>
2025-10-25 20:06:00 -0600

    add: Agregando clase Rol

---
+++ EmpresaAPI/Models/Rol.cs
@@ -0,0 +1,13 @@
+﻿namespace EmpresaAPI.Models
+{
+    public class Rol : AuditoriaBase
+    {
+        public int Id_Rol { get; set; }
+        public string Nombre_Rol { get; set; }
+        public bool Estado { get; set; } = true;
+
+        public ICollection<Usuario> Usuarios { get; set; } = new List<Usuario>();
+
+
+    }
+}
