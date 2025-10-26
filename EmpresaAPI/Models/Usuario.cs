35809cfd5f639f3d9e1d2f64ed81c7793d37692a
Lisbeth Hernandez <105689509+Hgliss@users.noreply.github.com>
2025-10-25 20:06:09 -0600

    add: Agregando clase Usuario

---
+++ EmpresaAPI/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿namespace EmpresaAPI.Models
+{
+    public class Usuario : AuditoriaBase
+    {
+        public int Id_Usuario { get; set; }
+        public string Nombre_Usuario { get; set; }
+        public string Contrasena { get; set; } = null!;
+        public bool Estado { get; set; } = true;
+
+        public int Empleado_Id { get; set;}
+        public Empleado Empleado { get; set; }
+
+        public int Rol_Id { get; set; }
+        public Rol Rol { get; set; }
+
+    }
+}
