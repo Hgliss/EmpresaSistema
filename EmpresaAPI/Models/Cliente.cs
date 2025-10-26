c5f006249635763d5c69dbe70887966e14337062
Lisbeth Hernandez <105689509+Hgliss@users.noreply.github.com>
2025-10-25 18:01:07 -0600

    add: Agregando Clase Cliente

---
+++ EmpresaAPI/Models/Cliente.cs
@@ -0,0 +1,13 @@
+﻿namespace EmpresaAPI.Models
+{
+    public class Cliente : AuditoriaBase
+    {
+        public int Id_Cliente { get; set; }
+        public string Nit { get; set; }
+        public string Nombre { get; set; }
+
+        public ICollection<Venta> Ventas { get; set; } = new List<Venta>();
+
+
+    }
+}
