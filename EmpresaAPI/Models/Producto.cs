c1524dbdcca3b19e14573d3725933653e6c3aa7b
Lisbeth Hernandez <105689509+Hgliss@users.noreply.github.com>
2025-10-25 19:39:00 -0600

    add: Agregando clase Producto

---
+++ EmpresaAPI/Models/Producto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmpresaAPI.Models
+{
+    public class Producto : AuditoriaBase
+    {
+        public int SKU { get; set; }
+        [MaxLength(255)]
+        public string Descripcion {  get; set; }
+        public int Existencia { get; set; }
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Precio { get; set; }
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Costo { get; set; }
+
+        public ICollection<DetalleVenta> DetallesVentas { get; set; } = new List<DetalleVenta>();
+
+    }
+}
