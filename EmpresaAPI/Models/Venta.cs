ef98d07d73069394640f18f374b28e07300d8e60
Lisbeth Hernandez <105689509+Hgliss@users.noreply.github.com>
2025-10-25 19:39:11 -0600

    add: Agregando clase Venta

---
+++ EmpresaAPI/Models/Venta.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmpresaAPI.Models
+{
+    public class Venta : AuditoriaBase
+    {
+        public int Id_Venta { get; set; }
+        public DateTime Fecha_Venta { get; set; }
+        [Column(TypeName ="decimal(10,2)")]
+        public int Total { get; set; }
+
+        public int Cliente_Id { get; set; }
+        public Cliente Cliente { get; set; } = null!;
+
+        public ICollection<DetalleVenta> DetallesVentas { get; set; } = new List<DetalleVenta>();
+    }
+}
