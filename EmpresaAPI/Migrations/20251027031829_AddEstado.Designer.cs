a25d3ab16fbdb052981fbedade0306a15b1fb4f3
Lisbeth Hernandez <105689509+Hgliss@users.noreply.github.com>
2025-10-26 21:22:02 -0600

    add: agregando campo Estado a tabla cliente y producto

--- EmpresaAPI/Migrations/20251027031829_AddEstado.Designer.cs
+++ EmpresaAPI/Migrations/20251027031829_AddEstado.Designer.cs
@@ -4,6 +4,7 @@ using EmpresaAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EmpresaAPI.Migrations
 {
     [DbContext(typeof(EmpresaContext))]
-    partial class EmpresaContextModelSnapshot : ModelSnapshot
+    [Migration("20251027031829_AddEstado")]
+    partial class AddEstado
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EmpresaAPI/Migrations/20251027031829_AddEstado.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmpresaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEstado : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Estado",
+                table: "Productos",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Estado",
+                table: "Clientes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Estado",
+                table: "Productos");
+
+            migrationBuilder.DropColumn(
+                name: "Estado",
+                table: "Clientes");
+        }
+    }
+}
