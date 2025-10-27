3461b271ba586b1a7799abd01f40b6f2fe5c3e6a
Lisbeth Hernandez <105689509+Hgliss@users.noreply.github.com>
2025-10-26 20:21:04 -0600

    add: Dtos Depto

---
+++ EmpresaAPI/DTOs/DepartamentoDto.cs
@@ -0,0 +1,11 @@
+﻿namespace EmpresaAPI.DTOs
+{
+    public class DepartamentoDto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal Presupuesto { get; set; }
+        public bool Estado { get; set; } = true;
+
+    }
+}
---
+++ EmpresaAPI/DTOs/DepartamentoInputDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmpresaAPI.DTOs
+
+{
+    public class DepartamentoInputDto
+    {
+        [Required(ErrorMessage = "El nombre es Obligatorio")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El presupuesto es Obligatorio")]
+        public decimal Presupuesto { get; set; }
+
+        [Required(ErrorMessage = "El estado es Obligatorio")]
+        public bool Estado { get; set; } = true;
+
+
+    }
+}
