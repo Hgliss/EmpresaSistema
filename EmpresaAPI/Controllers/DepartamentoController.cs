cefb1b4e36a43b5607eb5b5fe02ba4e25238a927
Lisbeth Hernandez <105689509+Hgliss@users.noreply.github.com>
2025-10-26 20:20:50 -0600

    add: DepartmentoController

---
+++ EmpresaAPI/Controllers/DepartamentoController.cs
@@ -0,0 +1,46 @@
+﻿using EmpresaAPI.DTOs;
+using EmpresaAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmpresaAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DepartamentoController : ControllerBase
+    {
+
+        private readonly DepartamentoService _service;
+
+        public DepartamentoController(DepartamentoService service)
+        {
+            _service = service;
+        }
+
+        //GET: api/departamentos
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            var departamentos = await _service.GetAllAsync();
+            return Ok(departamentos);
+        }
+
+        //POST: api/departamentos
+        [HttpPost]
+        public async Task<IActionResult> Create(DepartamentoInputDto dto)
+        {
+            await _service.CreacteAsync(dto);
+            return Ok(new { message = "Departamento creado correctamente" });
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var eliminado = await _service.DeleteAsync(id);
+            if (!eliminado) return NotFound(new { mesagge = "Departamento no encontrado" });
+            return Ok(new { message = "Departamento eliminado correctamente" });
+
+        }
+
+
+    }
+}
