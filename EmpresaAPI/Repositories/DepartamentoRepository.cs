220a7e0754f6b1f33931ad11ec39f7cb81ff0abd
Lisbeth Hernandez <105689509+Hgliss@users.noreply.github.com>
2025-10-26 20:21:36 -0600

    add: service y repository de departamento

---
+++ EmpresaAPI/Repositories/DepartamentoRepository.cs
@@ -0,0 +1,49 @@
+﻿using EmpresaAPI.Data;
+using EmpresaAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmpresaAPI.Repositories
+{
+    public class DepartamentoRepository
+    {
+        private readonly EmpresaContext _context;
+
+        public DepartamentoRepository(EmpresaContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Departamento>> GetAllAsync()
+        {
+            return await _context.Departamentos.ToListAsync();
+        }
+
+        public async Task AddAsync(Departamento departamento)
+        {
+            await _context.Departamentos.AddAsync(departamento);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Departamento?> GetByIdAsync(int id)
+        {
+            return await _context.Departamentos.FindAsync(id);
+        }
+
+        public async Task UpdateAsync(Departamento departamento)
+        {
+            _context.Departamentos.Update(departamento);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var departamento = await _context.Departamentos.FindAsync(id);
+            if (departamento != null)
+            {
+                _context.Departamentos.Remove(departamento);
+                await _context.SaveChangesAsync();
+            }
+
+        }
+    }
+}
---
+++ EmpresaAPI/Services/DepartamentoService.cs
@@ -0,0 +1,53 @@
+﻿using EmpresaAPI.DTOs;
+using EmpresaAPI.Models;
+using EmpresaAPI.Repositories;
+
+
+namespace EmpresaAPI.Services
+{
+    public class DepartamentoService
+    {
+        private readonly DepartamentoRepository _repo;
+
+        public DepartamentoService(DepartamentoRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<IEnumerable<DepartamentoDto>> GetAllAsync()
+        {
+            var departamento = await _repo.GetAllAsync();
+            return departamento.Select(d => new DepartamentoDto
+            {
+                Id = d.Id,
+                Nombre = d.Nombre,
+                Presupuesto = d.Presupuesto,
+                Estado = d.Estado,
+            });
+        }
+
+        public async Task CreacteAsync(DepartamentoInputDto dto)
+        {
+            var departamento = new Departamento
+            {
+                Nombre = dto.Nombre,
+                Presupuesto = dto.Presupuesto,
+                Estado = dto.Estado,
+                Fecha_Creacion = DateTime.UtcNow,
+                Fecha_Modificacion = DateTime.UtcNow,
+                Usuario_Creacion_Id = null,
+                Usuario_Modificacion_Id = null,
+            };
+            await _repo.AddAsync(departamento);
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var departamento = await _repo.GetByIdAsync(id);
+            if (departamento == null) return false;
+
+            await _repo.DeleteAsync(id);
+            return true;
+        }
+    }
+}
